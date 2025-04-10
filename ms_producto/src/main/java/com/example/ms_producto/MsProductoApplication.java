f2756d18470cfce71ece469da61b2e5170ac5c89
unknown <andresmorenochechomoreno1995@gmail.com>
2025-04-10 07:59:50 -0500

    mi segundo commit

--- ms_producto/src/main/java/com/example/ms_producto/MsProductoApplication.java
+++ ms_producto/src/main/java/com/example/ms_producto/MsProductoApplication.java
@@ -1,9 +1,10 @@
-package com.example.msproducto;
+package com.example.ms_producto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class MsProductoApplication {
 
     public static void main(String[] args) {
--- ms_producto/src/test/java/com/example/ms_producto/MsProductoApplicationTests.java
+++ ms_producto/src/test/java/com/example/ms_producto/MsProductoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.msproducto;
+package com.example.ms_producto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
